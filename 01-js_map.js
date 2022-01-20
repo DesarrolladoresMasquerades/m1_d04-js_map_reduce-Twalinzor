7febc1b1c6d09b75b5500f74a73efca7612805c1
Stan Tapchylka <stanislautapchylka@gmail.com>
2022-01-20 16:46:02 +0100

    Done

--- 01-js_map.js
+++ 01-js_map.js
@@ -1,1 +1,49 @@
 // Class code examples
+
+const names = ["Marco", "Carlos", "Carol"];
+
+const capitalize = (string) => {
+  return string.toUpperCase();
+};
+
+const allCaps = names.map(capitalize); // ["Marco", "Carlos", "Carol"].map()
+
+console.log(names, allCaps);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// const SomeCallbackFunction = ()=>{}
+const double = numbers.map((element) => {
+  return element * 2;
+}); //numbers.map (someCallbackFunction)
+
+console.log(double);
+
+const students = [
+  { name: "Rafa", color: "Blue", age: 12 },
+  { name: "Elise", color: "Pink", age: 21 },
+  { name: "Bodei", color: "Yellow", age: 31 },
+  { name: "Jorge", color: "Red", age: 11 },
+];
+
+const greetings = students.map((student) => {
+  return `Hello ${student.name} you are ${student.age} and your fav color is ${student.color}!`;
+});
+console.log(greetings);
+
+greetings.forEach((el) => console.log(el));
+
+console.log(["Marco", "Carlos", "Carol"].map(() => {}).length);
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+numbers2
+  .map((num) => {
+    return num * 2;
+  })
+  .filter((num) => {
+    return num > 5;
+  })
+  .sort((num1, num2) => {
+    return num1 - num2;
+  });
--- 02-js_reduce.js
+++ 02-js_reduce.js
@@ -1,1 +1,36 @@
 // Class code examples
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const callback = (acc, ele) => {
+  acc += ele;
+  return acc; //MUST ALWAYS RETURN ACC
+};
+
+const sum = numbers.reduce(
+  callback,
+  0 // Starting value of the accumulator is the second parameter
+);
+
+const students = [
+  { firstName: "John", lastName: "Carr", age: 34 },
+  { firstName: "Leonardo", lastName: "Di Vittorio", age: 21 },
+  { firstName: "Jessy", lastName: "Mc Scrudge", age: 99 },
+  { firstName: "Lisa", lastName: "Parker", age: 22 },
+  { firstName: "Marco", lastName: "Santo", age: 48 },
+];
+
+// I would like to create an obj with reduce that has properties that summarize the content of students array
+const countOlderThan30 = students.reduce((count, student) => {
+  if (student.age > 30) return (count += 1);
+  else return count;
+}, 0);
+
+console.log(countOlderThan30);
+
+const classRoster = students.reduce((roster, student) => {
+  roster.push(`${student.firstName} ${student.lastName}`);
+  return roster;
+}, []);
+
+console.log("roster: ", classRoster);
